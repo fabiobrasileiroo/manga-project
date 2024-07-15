d7f7d3b2dd89fc13111d4a26cb99d481117d864e
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-07-15 10:16:30 -0400

    estudo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 // Habilita o CORS para todas as origens
 app.use(cors());
